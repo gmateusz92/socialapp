0cc279ce176c91a2d01347220f819e99d421463b
mateusz <matek1600@o2.pl>
2022-11-30 15:10:07 +0100

    1

--- users/urls.py
+++ users/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('login/', views.user_login, name='login'), #ustawiam w settings
     path('logout/', auth_view.LogoutView.as_view(template_name='logout.html'), name = 'logout'), #ustawiam w settings
     path('password_change/', auth_view.PasswordChangeView.as_view(template_name='password_change_form.html'), name='password_change'),
-    path('password_change/done', auth_view.PasswordChangeDoneView.as_view(template_name='password_change_done'), name='password_change_done')
+    path('password_change/done', auth_view.PasswordChangeDoneView.as_view(template_name='password_change_done.html'), name='password_change_done'),
+    path('password_reset/', auth_view.PasswordResetView.as_view(template_name='password_reset_form.html'), name='password_reset')
     ]
