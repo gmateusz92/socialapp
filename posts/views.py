4f33c3e3d803368b7933b7a3820d68edb5f01dd2
mateusz <gmateusz92@gmail.com>
2023-01-02 19:45:33 +0100

    2

--- posts/views.py
+++ posts/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 from .forms import PostCreateForm
 from django.contrib.auth.decorators import login_required
+from .models import Post
+from django.shortcuts import get_object_or_404
 
 @login_required
 def post_create(request):
@@ -13,3 +15,15 @@ def post_create(request):
     else:
         form = PostCreateForm(request.GET)
     return render(request, 'posts/create.html', {'form':form})
+
+def feed(request):
+    posts = Post.objects.all()
+    return render(request, 'posts/feed.html', {'posts': posts})
+
+def like_post(request):
+    post_id = request.POST.get('post_id')
+    post = get_object_or_404(Post, id=post_id)
+    if post.liked_by.filter(id=request.user.id).exists():
+        post.liked_by.remove(request.user)
+    else:
+        post.liked_by.add(request.user)    
